e93fe1ebd4a8841cfcddcb5060a83a64dc1cade9
Zenith-Convoy <allanparkerzero@gmail.com>
2019-10-25 19:04:58 +0100

    creation

---
+++ leetcode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSum twoSum = new TwoSum();
+            foreach(int i in twoSum.main(new int[] { 3, 3}, 6))
+            {
+                Console.WriteLine(i.ToString());
+            }
+           
+        }
+        
+    }
+}
---
+++ leetcode/TwoSum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode
+{
+    class TwoSum
+    {
+        public int[] main(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int e = i + 1; e < nums.Length; e++)
+                {
+                    if (nums[i] + nums[e] == target)
+                    {
+                        return new int[2] { i, e };
+                    }
+                }
+            }
+            return new int[2] { -1, -1 };
+        }
+    }
+}
